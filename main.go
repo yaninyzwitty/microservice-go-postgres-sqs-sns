c3aba4a4dd226e3cdcabd1ab53f62035d2913696
yaninyzwitty <ianmwa143@gmail.com>
2024-10-30 16:12:54 +0300

    add repeating code under one folder

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 	"github.com/yaninyzwitty/sqs-go/internal/repository"
 	"github.com/yaninyzwitty/sqs-go/internal/router"
 	"github.com/yaninyzwitty/sqs-go/internal/service"
+	"github.com/yaninyzwitty/sqs-go/shared"
 )
 
 func main() {
@@ -23,7 +24,7 @@ func main() {
 	defer cancel()
 
 	var cfg pkg.Config
-	if err := loadConfig(&cfg); err != nil {
+	if err := shared.LoadConfig(&cfg); err != nil {
 		slog.Error("failed to load configuration", "error", err)
 		os.Exit(1)
 	}
@@ -62,39 +63,12 @@ func main() {
 		Handler: mux,
 	}
 
-	go startServer(server)
+	go shared.StartServer(server)
 
 	slog.Info("server is running at port", "port", cfg.Server.PORT)
 	quitCH := make(chan os.Signal, 1)
 	signal.Notify(quitCH, os.Interrupt)
 
 	<-quitCH
-	shutdownServer(server)
-}
-
-func loadConfig(cfg *pkg.Config) error {
-	file, err := os.Open("config.yaml")
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	return cfg.LoadConfig(file)
-}
-
-func startServer(server *http.Server) {
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		slog.Error("failed to start server, server error", "error", err)
-		os.Exit(1)
-	}
-}
-
-func shutdownServer(server *http.Server) {
-	slog.Info("Received termination signal, shutting down server...")
-	shutdownCTX, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	if err := server.Shutdown(shutdownCTX); err != nil {
-		slog.Error("Failed to gracefully shut down server", "error", err)
-	}
-	slog.Info("Server shutdown successful")
+	shared.ShutdownServer(server)
 }
